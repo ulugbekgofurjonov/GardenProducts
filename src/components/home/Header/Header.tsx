cc4e8bb625f9ba979723ff2537873faa7d7cd30a
Ulug'bek G'ofurjonov <ulugbekgofurjonov001@gmail.com>
2026-01-09 17:47:37 +0500

    Modern and responsive e-commerce frontend built with Next.js. Converted from Figma design using a component-based architecture, dynamic routing, and interactive UI features.

---
+++ src/components/home/Header/Header.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import Link from "next/link";
+import { useState } from "react";
+import './Header.css'
+
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <div className='header container'>
+      {/* Logo */}
+      <Link href="/">
+        <img 
+          className='img' 
+          src="./Images/HomePage Images/logo.png" 
+          alt="Logo" 
+        />
+      </Link>
+
+      {/* Navigation Menu */}
+      <nav className={`nav-wrapper ${isMenuOpen ? 'active' : ''}`}>
+        <ul className="nav-list">
+          <li><Link className="link" href="/" onClick={closeMenu}>Main Page</Link></li>
+          <li><Link className="link" href="/categories" onClick={closeMenu}>Categories</Link></li>
+          <li><Link className="link" href="/products" onClick={closeMenu}>All products</Link></li>
+          <li><Link className="link" href="/sales" onClick={closeMenu}>All sales</Link></li>
+        </ul>
+      </nav>
+
+      {/* Right Side */}
+      <div className="header-right">
+        {/* Cart Icon */}
+        <Link href="/cart">
+          <img 
+            src="./Images/HomePage Images/savat.svg" 
+            alt="Cart"
+            className="cart-icon"
+          />
+        </Link>
+
+        {/* Hamburger Menu Icon */}
+        <div 
+          className={`menu-icon ${isMenuOpen ? 'active' : ''}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
+}

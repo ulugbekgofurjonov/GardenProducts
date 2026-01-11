f0f3ab9352f0190d9c62b93c89d8e85ea5224cb8
Ulug'bek G'ofurjonov <ulugbekgofurjonov001@gmail.com>
2026-01-11 13:01:50 +0500

    I created a new Sale folder in the Components section and created a Sale.tsx file in it and laid out the structure in it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Banner from "@/components/home/Banner/Banner";
 import Categories from "@/components/home/Categories/Categories";
 import Header from "@/components/home/Header/Header";
 import Order from "@/components/home/Order/Order";
+import Sale from "@/components/home/Sale/Sale";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Banner/>
       <Categories/>
       <Order/>
+      <Sale/>
     </div>
   );
 }
---
+++ src/components/home/Sale/Sale.tsx
@@ -0,0 +1,19 @@
+export default function Sale() {
+  return (
+    <div className="sale">
+      <div className="top">
+        <h2>Sale</h2>
+        <h3>All sales</h3>
+      </div>
+      <div className="wrapper">
+        <div className="box">
+          <img src="./Images/HomePageIMG/sale1.png" alt="" />
+          <h5>Decorative forged bridge</h5>
+          <h3>
+            $500 <span>$1000</span>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+}
